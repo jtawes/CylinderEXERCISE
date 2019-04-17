04c405c7b4b704c6580ee275dcf1fed21c9f335f
jtawes <[jtawes510@gmail.com]>
2019-04-17 11:36:59 -0400

    Second commit

--- src/com/example/java/Circle.java
+++ src/com/example/java/Circle.java
@@ -5,8 +5,14 @@ public class Circle {
     private double radius;
 
     public Circle(double radius) {
-        this.radius = radius;
+        if(radius < 0 ) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+
     }
 
-    
+
 }
+
